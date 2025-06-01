3cf17011c4175d6a894863bda95e7bf1688ee065
leonardo kusma <leonardokusma48@gmail.com>
2025-06-01 17:56:02 -0300

    [FIX] carrosel

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+let count = 1;
+
+document.getElementById("radio1").checked = true
+
+setInterval(function(){
+    nextImagem()
+},20000)
+
+function nextImagem(){
+    count ++;
+    if(count>5){
+        count = 1;
+    }
+
+    document.getElementById("radio" + count).checked = true;
+}
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
     initializeApp();
 });
-
 // ===== MAIN INITIALIZATION =====
 function initializeApp() {
     setupFormHandler();
